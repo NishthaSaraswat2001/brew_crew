d71df21eab2dc5afb7ac20fd7928fc82f88e92b9
Nishtha <nishthasaraswat15@gmail.com>
2021-06-09 22:24:44 +0530

    brew crew app

---
+++ lib/screens/authenticate/authenticate.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+ class Authenticate extends StatefulWidget {
+   @override
+   AauthenticateState createState() => AauthenticateState();
+ }
+ 
+ class AauthenticateState extends State<Authenticate> {
+   @override
+   Widget build(BuildContext context) {
+     return Container(
+       child: Text("Authenticate"),
+     );
+   }
+ }
