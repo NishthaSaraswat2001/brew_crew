ea1a8856c7ce02440388b71392b6ade78d760bb4
Nishtha <nishthasaraswat15@gmail.com>
2021-06-12 22:39:58 +0530

    stream builder to toggle b/w home & authenticate screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,39 @@
 import 'package:brew_crew/screens/Wrapper.dart';
+import 'package:brew_crew/screens/authenticate/authenticate.dart';
+import 'package:brew_crew/screens/home/home.dart';
+import 'package:brew_crew/services/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     home: Wrapper(),
+      home: StreamBuilder(
+          stream: _auth.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return Home();
+            } else {
+              return Authenticate();
+            }
+          }),
     );
+
+    // StreamProvider<SingInAnonyUser>.value(
+    //   value: AuthServices().user;
+    //   child: MaterialApp(
+    //    home: Wrapper(),
+    //   ),
+    //);
   }
 }
-
