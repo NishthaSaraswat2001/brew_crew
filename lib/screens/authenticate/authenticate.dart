d5e15842aa737e4dccd6b0b190f9e6206eb588ee
Nishtha <nishthasaraswat15@gmail.com>
2021-06-12 22:35:50 +0530

    toggle b/w register & signIn widgets

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,14 +1,26 @@
+import 'package:brew_crew/screens/authenticate/registration.dart';
+import 'package:brew_crew/screens/authenticate/sign_in.dart';
 import 'package:flutter/material.dart';
- class Authenticate extends StatefulWidget {
-   @override
-   AauthenticateState createState() => AauthenticateState();
- }
- 
- class AauthenticateState extends State<Authenticate> {
-   @override
-   Widget build(BuildContext context) {
-     return Container(
-       child: Text("Authenticate"),
-     );
-   }
- }
+
+class Authenticate extends StatefulWidget {
+  @override
+  AauthenticateState createState() => AauthenticateState();
+}
+
+class AauthenticateState extends State<Authenticate> {
+  bool isSignIn = true;
+  void toggleView() {
+    setState(() {
+      isSignIn = !isSignIn;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isSignIn) {
+      return SignIn(toggleView: toggleView);
+    } else {
+      return Register(toggleView: toggleView);
+    }
+  }
+}
