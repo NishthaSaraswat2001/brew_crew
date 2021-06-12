5f2ace39941c957ba30037d9e731609749e296f0
Nishtha <nishthasaraswat15@gmail.com>
2021-06-12 22:41:08 +0530

    signOut functionality in appbar button

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,11 +1,29 @@
+import 'package:brew_crew/services/auth.dart';
 import 'package:flutter/material.dart';
 
-
 class Home extends StatelessWidget {
+  final AuthServices _auth = AuthServices();
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text("home"),
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Brew Crew'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
+              icon: Icon(Icons.person),
+              label: Text('logout'),
+              onPressed: () async {
+                await _auth.signOut();
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
